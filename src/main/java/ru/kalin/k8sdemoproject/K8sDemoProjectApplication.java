2fe2929dd4a24cf8a964b3c64a1a76c1d8ad5222
a.k.lysenko <a.k.lysenko@tbank.ru>
2024-07-29 13:17:27 +0500

    initial commit

---
+++ src/main/java/ru/kalin/k8sdemoproject/K8sDemoProjectApplication.java
@@ -0,0 +1,11 @@
+package ru.kalin.k8sdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sDemoProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(K8sDemoProjectApplication.class, args);
+    }
+}
